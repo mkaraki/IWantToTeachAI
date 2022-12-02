97a2d3a21a60ba831d17e5134d5e689e04176302
mkaraki <masakazu.araki@mkarakiapps.com>
2022-12-02 21:49:12 +0900

    Change title

--- teach.php
+++ teach.php
@@ -16,7 +16,7 @@ $srvSrcPath = '/dataset/' . $tid . '/src/' . urlencode($teachInfo['src'][$randSr
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teach - I Want To Teach</title>
+    <title>I Want To Teach AI</title>
     <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 </head>
 
